62c82c775ab7f76c89275e69b673d5cb14257e5c
idiaz97 <idiaz@itba.edu.ar>
2019-01-31 22:00:25 -0300

    changes on game

--- Game.cpp
+++ Game.cpp
@@ -4,6 +4,7 @@ Game::Game() {
 	Graphics screen;
 	Player player;
 	player.setPlayer(screen.getMap().getTeam(), &screen.getMap());
+	srand(time(NULL));
 }
 
 void Game::run() {
@@ -68,7 +69,7 @@ void Game::run() {
 			}
 		}
 		else {
-
+			net.waitForMyTurn(&this->callback, &this->callbackResponseAttack);
 		}
 	}
 
@@ -82,15 +83,26 @@ void * Game::callbackClient(const char* mapName, unsigned int mapNameSize, int c
 
 bool Game::callback(move_s move, int data1, int data2, int data3, int data4, int data5) {
 	bool answer = false;
-	if (move != ATTACK)
-		return false;
-	else {
+	if (move == ATTACK){
 		Position pos(data1, data2);
 		Position pos2(data3, data4);
 		answer = player.getMap()->enemyAttack(player.getMap()->getUnit(pos), pos2, data5);
+		screen.showDices(myDice, data5);
 	}
-	screen.showDices(myDice, data5);
+	else if (move == PURCHASE) {
+		string code;
+		code.push_back(data1);
+		code.push_back(data2);
+		units_d unit = getUnitFromCode(code);
+		Position pos(data3, data4);
+		player.getMap()->addUnit(unit, pos, player.getMap()->getEnemyTeam());
+		answer = true;
+	}
 	return answer;
+}
+
+int Game::callbackResponseAttack(void) {
+	return rand() % 6 + 1;
 }
 
 string Game::getUnitCode(units_d unitClass)
--- Game.h
+++ Game.h
@@ -18,8 +18,10 @@ protected:
 private:
 	void * callbackClient(const char* mapName, unsigned int mapNameSize, int checksum);
 	bool callback(move_s move, int data1, int data2, int data3, int data4, int data5);
+	int callbackResponseAttack(void);
 	unsigned int myDice;
 	string getUnitCode(units_d unitClass);
+
 
 };
 
