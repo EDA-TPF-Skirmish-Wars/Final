acd012032d99b81279a68846b7ca6b41c4208904
mmaspero <mmaspero@itba.edu.ar>
2019-01-31 21:37:06 -0300

    a

--- Game.h
+++ Game.h
@@ -5,6 +5,7 @@
 #include "./Network/Connections.h"
 #include "./Game/classMap.h"
 #include "./Game/classPlayer.h"
+#include <string>
 
 class Game {
 public:
@@ -18,6 +19,8 @@ private:
 	void * callbackClient(const char* mapName, unsigned int mapNameSize, int checksum);
 	bool callback(move_s move, int data1, int data2, int data3, int data4, int data5);
 	unsigned int myDice;
+	string getUnitCode(units_d unitClass);
+
 };
 
 #endif
